012e29eb4e691326620108ee3efa96e73f0f691c
Arthur Dos Santos Sousa <102875535+KiritoGamesPlays@users.noreply.github.com>
2026-02-12 15:37:35 -0300

    Change default port from 3000 to 9005

--- api/index.ts
+++ api/index.ts
@@ -28,7 +28,7 @@ export default handle(app)
 
 // Run local development server when not on Vercel
 if (!process.env.VERCEL) {
-  const port = parseInt(process.env.PORT || '3000')
+  const port = parseInt(process.env.PORT || '9005')
   serve(
     {
       fetch: app.fetch,
